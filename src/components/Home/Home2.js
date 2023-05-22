5e54c112228d5e27dd6a9d7d0a99754691e8e37b
Arun <arunislordofgod49@gmail.com>
2023-05-22 15:23:09 +0530

    Add: profile pic to home screen

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/arun_profile.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -72,7 +72,11 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={myImg} 
+              className="img-fluid" 
+              style={{
+                borderRadius: "30px"
+              }} alt="avatar" />
             </Tilt>
           </Col>
         </Row>
